f81947f0bb98ce3534805266f29d315898507e57
devran1 <40788986+devran1@users.noreply.github.com>
2022-03-31 17:45:00 +0300

    Add files via upload

---
+++ not_done/add-text.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+
+import alphabet
+
+
+
+import argparse
+
+
+parser=argparse.ArgumentParser(description="adds text to images")
+parser.add_argument("text",help="add video file to rotate",type=str)
+
+args=parser.parse_args()
+
+
+sentence=args.text
+
+for word in sentence:
+    #print(word)
+    #each_word=alphabet.words(list(word))#not working
+    each_word=alphabet.words(word)
+    print(each_word)
+    each_W=np.array(each_word,dtype=np.uint8)
+#    cv2.imshow('white', each_W)
+
+#from alphabet import alpha
+
+r,c = 360, 720
+
+
+#numpy array
+white = np.full((r,c,3), 255, dtype=np.uint8)
+ 
+#show image
+#cv2.imshow('white', white)
+
+#print(alphabet.a) # matrix
+
+#letter_a=np.array((alphabet.a,3),255,dtype=np.uint8)
+
+ 
+
+cv2.waitKey(0) # waits until a key is pressed
+cv2.destroyAllWindows() # destroys the window showing image
+
