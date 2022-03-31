25136ac4cbaddc82e79f2a83ea6be7aeac58fdf0
devran1 <40788986+devran1@users.noreply.github.com>
2022-03-31 18:56:43 +0300

    Add files via upload

--- not_done/add-text.py
+++ not_done/add-text.py
@@ -20,13 +20,14 @@ sentence=args.text
 for word in sentence:
 
     i=alphabet.letters_list.index(f"{word}")
-    print(i)
+    #print(i)
     new_word=alphabet.letters[i]
 
 
     each_word=alphabet.words(new_word)
     
     each_W=np.array(each_word,dtype=np.uint8)
+    #print(each_W) #each word 7x7 place in 360x720
     
     #cv2.imshow('white', each_W)
 
@@ -34,17 +35,17 @@ for word in sentence:
 r,c = 360, 720
 
 
-#numpy array
-white = np.full((r,c,3), 255, dtype=np.uint8)
- 
-#show image
-#cv2.imshow('white', white)
+#making screen 360x720
+screen=[[0 for col in range(c)] for rows in range(r)] 
 
-#print(alphabet.a) # matrix
+for i in range(r):
+    for j in range(c):
+        screen[i][j]=[255,255,255]
 
-#letter_a=np.array((alphabet.a,3),255,dtype=np.uint8)
+screen_array=np.array(screen,dtype=np.uint8)
+cv2.imshow('white', screen_array)
 
- 
+
 
 cv2.waitKey(0) # waits until a key is pressed
 cv2.destroyAllWindows() # destroys the window showing image
